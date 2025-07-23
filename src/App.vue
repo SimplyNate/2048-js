f70a6b9f2328f30b3c25deb1e7f932ea15e918ff
SimplyNate <horiuchi.nathan@gmail.com>
2025-07-23 16:17:59 -0700

    Define background instead of showing blanks as 0

--- src/App.vue
+++ src/App.vue
@@ -51,10 +51,26 @@ onMounted(() => {
         <div>Last Move: {{ lastMove }}</div>
         <div>Game State: {{ game.currentGameState }}</div>
         <button @click="resetGame">Reset</button>
-        <div class="mt-5 fs-2 fw-bold">
+        <div class="mt-5 fs-2 fw-bold" style="border: 1px solid green;">
+            <div class="d-flex justify-content-center position-relative" v-for="i in 4" :key="i">
+                <div class="border border-dark bg-0 w-5 position-relative">
+                    <div :class="`position-absolute top-0 left-0 w-5 border border-dark bg-${game.state[i-1][0]}`" v-if="game.state[i-1][0] > 0">{{ game.state[i-1][0] }}</div>
+                </div>
+                <div class="border border-dark bg-0 w-5 position-relative">
+                    <div :class="`position-absolute top-0 left-0 w-5 border border-dark bg-${game.state[i-1][1]}`" v-if="game.state[i-1][1] > 0">{{ game.state[i-1][1] }}</div>
+                </div>
+                <div class="border border-dark bg-0 w-5 position-relative">
+                    <div :class="`position-absolute top-0 left-0 w-5 border border-dark bg-${game.state[i-1][2]}`" v-if="game.state[i-1][2] > 0">{{ game.state[i-1][2] }}</div>
+                </div>
+                <div class="border border-dark bg-0 w-5 position-relative">
+                    <div :class="`position-absolute top-0 left-0 w-5 border border-dark bg-${game.state[i-1][3]}`" v-if="game.state[i-1][3] > 0">{{ game.state[i-1][3] }}</div>
+                </div>
+            </div>
+            <!--
             <div class="d-flex justify-content-center" v-for="i in 4" :key="i">
                 <div v-for="j in 4" :key="j" :class="`border border-dark w-5 bg-${game.state[i-1][j-1]}`">{{ game.state[i-1][j-1] > 0 ? game.state[i-1][j-1] : '' }}</div>
             </div>
+            -->
         </div>
     </div>
 </template>
